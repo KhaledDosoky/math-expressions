4025f67ac93a53572113efb773d9b89b7bbe0a89
Khaled Dosoky <khaled.wt@hotmail.com>
2025-11-26 17:30:19 -0800

    update PWA

--- frontend/convert-icons.js
+++ frontend/convert-icons.js
@@ -1,5 +1,9 @@
 import sharp from 'sharp';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 async function convertSvgToPng() {
   const publicDir = path.join(__dirname, 'public');
--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -7,7 +7,7 @@ export default function Document() {
         {/* PWA Meta Tags */}
         <meta name="application-name" content="Math Expressions" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="apple-mobile-web-app-title" content="MathExpr" />
         <meta name="description" content="Interactive mathematical expression evaluator" />
         <meta name="format-detection" content="telephone=no" />
@@ -15,7 +15,7 @@ export default function Document() {
         <meta name="msapplication-config" content="/browserconfig.xml" />
         <meta name="msapplication-TileColor" content="#000000" />
         <meta name="msapplication-tap-highlight" content="no" />
-        <meta name="theme-color" content="#000000" />
+        <meta name="theme-color" content="#9370DB" />
 
         {/* PWA Icons */}
         <link rel="apple-touch-icon" href="/icon-192x192.png" />
